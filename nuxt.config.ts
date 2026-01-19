aa84898451b369d1cb5264cc3f9ae03d6523fb97
Jon 久世 <jon@neorelm.com>
2026-01-19 13:53:25 -0500

    Add Nitro Preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,15 +20,19 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: '2025-01-15',
-
   nitro: {
     prerender: {
       routes: [
         '/'
       ]
+    },
+    preset: 'cloudflare-pages', // or "cloudflare-module" if using Workers
+    // Optionally enable native Node compatibility
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
     }
   },
-
   eslint: {
     config: {
       stylistic: {
